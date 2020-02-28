9a265a7ce3da68ad0db896a631aebaf394fd2a45
CrazyEric <1254297317@qq.com>
2020-02-28 13:44:47 +0800

    1.1 增加汤神灵魂签到提示音

--- 听说你们喜欢搞签到?.py
+++ 听说你们喜欢搞签到?.py
@@ -3,12 +3,16 @@
 import requests
 import re
 from threading import Timer
+import time
+import pygame
 
 #课程标识与班级标识，需手动填入，每门课不一样
 courseId=
 jclassId=
 #间隔时间，以秒为单位
-check_time=20
+check_time=
+
+filepath=r"汤老师的灵魂拷问.mp3"
 
 # 瞄准签到页面
 url = 'https://mobilelearn.chaoxing.com/widget/pcpick/stu/index?courseId='+str(courseId)+'&jclassId='+str(jclassId)
@@ -38,12 +42,21 @@ def qnmb():
     content=content.replace('\t','')
     #print(content)
     match=re.findall(r'<divid="startList">(.*?)</div>',content, re.S|re.M)
-    if match:
-        activeid=re.findall(r'onclick="activeDetail\((.*?),',match[0])
-        for x in activeid:
+    for s in match:
+        rect=re.findall(r'<ahref="javascript:;"shape="rect">\[(.*?)\]</a>',s)[0]
+        activeid=re.findall(r'onclick="activeDetail\((.*?),',s)[0]
+        if rect=='签到':
             sign_url='https://mobilelearn.chaoxing.com/widget/sign/pcStuSignController/preSign?activeId='+x+'&classId='+str(jclassId)+'&fid='+str(fid)+'&courseId='+str(courseId)
             print('签到成功')
             requests.get(sign_url,headers=header)
+        else:
+            #如果是位置签到或者手势签到，播放汤神灵魂提示音
+            pygame.mixer.init()
+            track = pygame.mixer.music.load(filepath)
+            pygame.mixer.music.play()
+            time.sleep(10)
+            pygame.mixer.music.stop()
+
 
 class RepeatingTimer(Timer):
     def run(self):
@@ -53,3 +66,5 @@ class RepeatingTimer(Timer):
 t = RepeatingTimer(check_time,qnmb)
 
 t.start()
+
+
